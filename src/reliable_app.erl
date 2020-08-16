4f2c4e01e4bc958a664b4573a5ec8637803a5ca1
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2020-08-16 18:05:58 +0100

    enable riakc bucket/key listing

--- src/reliable_app.erl
+++ src/reliable_app.erl
@@ -2,10 +2,11 @@
 
 -behaviour(application).
 
--export([start/2, 
+-export([start/2,
          stop/1]).
 
 start(_StartType, _StartArgs) ->
+    ok = application:set_env(riakc, allow_listing, true),
     reliable_sup:start_link().
 
 stop(_State) ->
