9f52d6e516bdf9131a51660fe6426b419c9da58b
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2020-09-25 11:25:23 +0100

    comments and formatting

--- src/reliable_app.erl
+++ src/reliable_app.erl
@@ -5,9 +5,21 @@
 -export([start/2,
          stop/1]).
 
+
+
+
+%% =============================================================================
+%% APPLICATION CALLBACKS
+%% =============================================================================
+
+
+
 start(_StartType, _StartArgs) ->
     ok = application:set_env(riakc, allow_listing, true),
-    reliable_sup:start_link().
+    case reliable_sup:start_link() of
+        {ok, _} = OK -> OK;
+        {error, _} = Error -> Error
+    end.
 
 stop(_State) ->
     ok.
--- src/reliable_sup.erl
+++ src/reliable_sup.erl
@@ -3,7 +3,6 @@
 -behaviour(supervisor).
 
 -export([start_link/0]).
-
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
@@ -12,8 +11,25 @@
         {I, {I, start_link, []}, permanent, Timeout, Type, [I]}).
 -define(CHILD(I, Type), ?CHILD(I, Type, 5000)).
 
+
+
+
+%% =============================================================================
+%% API
+%% =============================================================================
+
+
+
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+
+
+%% =============================================================================
+%% SUPERVISOT CALLBACKS
+%% =============================================================================
+
+
 
 init([]) ->
     SupFlags = #{strategy => one_for_all,
