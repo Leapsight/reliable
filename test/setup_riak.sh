706ac416b2f062aff649a2716c8b9e030cd29c9f
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2020-11-09 19:48:16 +0000

    fixed config
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- test/setup_riak.sh
+++ test/setup_riak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 docker exec -it riakkv riak-admin bucket-type create sets '{"props":{"datatype":"set", "n_val":3}}'
 docker exec -it riakkv riak-admin bucket-type activate sets
