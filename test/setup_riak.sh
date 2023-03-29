005d74586f5d87c33f5f97e3cbdbbccc13ae83a4
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2023-03-29 17:22:00 +0100

    updated riak docker name
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- test/setup_riak.sh
+++ test/setup_riak.sh
@@ -2,5 +2,5 @@
 
 # set -e
 
-docker exec -it riakkv riak-admin bucket-type create sets '{"props":{"datatype":"set", "n_val":3}}'
-docker exec -it riakkv riak-admin bucket-type activate sets
+docker exec -it fleet-riakkv riak-admin bucket-type create sets '{"props":{"datatype":"set", "n_val":3}}'
+docker exec -it fleet-riakkv riak-admin bucket-type activate sets
